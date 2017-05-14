2bfd810141663ac3b4f59ccfdb4402a0c44a381d
iurii-kyrylenko <yuriproject@gmail.com>
2017-05-14 13:01:22 +0300

    Code layout

--- client/src/App.js
+++ client/src/App.js
@@ -21,17 +21,17 @@ addMockFunctionsToSchema({ schema })
 const networkInterface = mockNetworkInterfaceWithSchema({ schema })
 const client = new ApolloClient({ networkInterface })
 
-const ChannelsList = ({ data: {loading, error, channels }}) => {
-   if (loading) {
-     return <p>Loading ...</p>
-   }
-   if (error) {
-     return <p>{error.message}</p>
-   }
-   return <ul>
-     { channels.map( ch => <li key={ch.id}>{ch.name}</li> ) }
-   </ul>
- }
+const ChannelsList = ({ data: { loading, error, channels }}) => {
+  if (loading) {
+   return <p>Loading ...</p>
+  }
+  if (error) {
+   return <p>{ error.message }</p>
+  }
+  return <ul> {
+    channels.map(ch => <li key={ ch.id }>{ ch.name }</li>)
+  } </ul>
+}
 
 const channelsListQuery = gql`
   query ChannelsListQuery {
@@ -47,10 +47,10 @@ const ChannelListWithData = graphql(channelsListQuery)(ChannelsList)
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
+      <ApolloProvider client={ client }>
         <div className="App">
           <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={ logo } className="App-logo" alt="logo" />
             <h2>Welcome to Apollo</h2>
           </div>
           <ChannelListWithData />
